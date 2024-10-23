0e7a522d43a74bf422b572ea84217263dec84246
Evgeny Gumenyuk <evgenij.iv.gumenyuk@gmail.com>
2024-10-23 17:50:00 +0300

    update history screen

--- Umbrella/screens/HistoryScreen.js
+++ Umbrella/screens/HistoryScreen.js
@@ -7,16 +7,16 @@ const HistoryScreen = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get('http://192.168.56.1:5000/history')
+    axios.get('http://192.168.56.1:5000/history')  
       .then(response => {
         setHistory(response.data); 
       })
       .catch(error => {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching history data:', error);
         setError(error);
       });
   }, []);
-  
+
   if (error) {
     return (
       <View style={styles.container}>
@@ -25,43 +25,18 @@ const HistoryScreen = () => {
     );
   }
 
-  // const [umbrellas] = useState([
-  //   {
-  //     _id: '1',
-  //     umbrella_id: 'U1',
-  //     location_id: 'B1',
-  //     sensor_id: 'B1S1',
-  //     user_id: 'null',
-  //     status: 'Available',
-  //   },
-  //   {
-  //     _id: '2',
-  //     umbrella_id: 'U2',
-  //     location_id: 'C2',
-  //     sensor_id: 'C2S1',
-  //     user_id: 'User002',
-  //     status: 'In Use',
-  //   },
-  //   {
-  //     _id: '3',
-  //     umbrella_id: 'U3',
-  //     location_id: 'E1',
-  //     sensor_id: 'E1S2',
-  //     user_id: 'null',
-  //     status: 'Available',
-  //   },
-  // ]);
-
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Your history:</Text>
+      <Text style={styles.title}>Rental History:</Text>
       <FlatList
+        data={history}
         keyExtractor={item => item._id}
         renderItem={({ item }) => (
           <View style={styles.item}>
-            <Text style={styles.itemText}> Umbrella ID: {item.umbrella_id}</Text>
-            <Text style={styles.itemText}>Slot number: {item.rented_at}</Text>
-            <Text style={styles.itemText}>Status: {item.rented_until}</Text>
+            <Text style={styles.itemText}>Umbrella ID: {item.umbrella_id}</Text>
+            <Text style={styles.itemText}>Rented at: {new Date(item.rented_at).toLocaleString()}</Text>
+            <Text style={styles.itemText}>Rented until: {new Date(item.rented_until).toLocaleString()}</Text>
+            <Text style={styles.itemText}>User ID: {item.user_id}</Text>
           </View>
         )}
       />
@@ -72,24 +47,24 @@ const HistoryScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20
+    padding: 20,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20
+    marginBottom: 20,
   },
   item: {
     padding: 10,
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc'
+    borderBottomColor: '#ccc',
   },
   itemText: {
-    fontSize: 20, 
+    fontSize: 20,
   },
   error: {
     color: 'red',
-    fontSize: 18
+    fontSize: 18,
   },
 });
 
