97df701f64eeb23a06c12125c4ec5db8dfc5278a
AndreiSusha <Andrey.Susha@gmail.com>
2024-10-24 10:44:39 +0300

    feat: api.js added

---
+++ Umbrella/utils/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_URL = ''; // URL of our API
+
+export const registerUser = async (userData) => {
+  const response = await axios.post(`${API_URL}/auth/register`, userData);
+  return response.data;
+};
+
+export const loginUser = async (userData) => {
+  const response = await axios.post(`${API_URL}/auth/login`, userData);
+  return response.data;
+};
