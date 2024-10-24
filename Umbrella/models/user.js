cc5d702780b270a33350444e9ac7be1eab31c99b
AndreiSusha <Andrey.Susha@gmail.com>
2024-10-24 11:11:06 +0300

    feat: user model added

---
+++ Umbrella/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    avatarUrl: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
