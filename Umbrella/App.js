c1455c372590e3bfd0c14a14e0d44374cb9fe3f1
AndreiSusha <Andrey.Susha@gmail.com>
2024-10-26 18:56:43 +0300

    feat: update with signup

--- Umbrella/App.js
+++ Umbrella/App.js
@@ -1,11 +1,34 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import BottomTabNavigator from "./navigation/BottomTabNavigator.js";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import BottomTabNavigator from './navigation/BottomTabNavigator.js';
+import WelcomeScreen from './screens/WelcomeScreen.js';
+import LoginScreen from './screens/LoginScreen.js';
+import SingupScreen from './screens/SignupScreen.js';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <BottomTabNavigator />
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={SingupScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Main" component={BottomTabNavigator} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
