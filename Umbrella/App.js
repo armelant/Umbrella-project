f044e627ffc4833f9ecf408d600588bf8519d662
AndreiSusha <Andrey.Susha@gmail.com>
2024-10-27 10:13:52 +0200

    feat: impotrs added

--- Umbrella/App.js
+++ Umbrella/App.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import ActiveRentalsScreen from './screens/ActiveRentalsScreen.js';
+import ProfileScreen from './screens/ProfileScreen.js';
+import HomeScreen from './screens/HomeScreen.js';
+import UmbrellasScreen from './screens/UmbrellasScreen.js';
+import VerifyEmailScreen from './screens/VerifyEmailScreen.js';
 import BottomTabNavigator from './navigation/BottomTabNavigator.js';
 import WelcomeScreen from './screens/WelcomeScreen.js';
 import LoginScreen from './screens/LoginScreen.js';
