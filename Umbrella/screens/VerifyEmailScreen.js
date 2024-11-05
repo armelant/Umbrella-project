f99fbfa6880424605bcc26d8353a263629fb0bd5
AndreiSusha <Andrey.Susha@gmail.com>
2024-11-05 18:20:02 +0200

    feat: styles added

--- Umbrella/screens/VerifyEmailScreen.js
+++ Umbrella/screens/VerifyEmailScreen.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  Alert,
+  TouchableOpacity,
+} from 'react-native';
 import axios from 'axios';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -47,7 +55,9 @@ const VerifyEmailScreen = ({ route }) => {
         value={confirmationCode}
         onChangeText={setConfirmationCode}
       />
-      <Button title="Verify" onPress={verifyEmail} />
+      <TouchableOpacity style={styles.button} onPress={verifyEmail}>
+        <Text style={styles.buttonText}>Verify</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -56,18 +66,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
     padding: 16,
+    backgroundColor: '#f5f5f5',
   },
   title: {
-    fontSize: 24,
-    marginBottom: 16,
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 24,
   },
   input: {
-    height: 40,
-    borderColor: 'gray',
+    height: 50,
+    width: '100%',
+    borderColor: '#ccc',
     borderWidth: 1,
-    marginBottom: 12,
-    paddingHorizontal: 10,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    marginBottom: 16,
+    backgroundColor: '#fff',
+  },
+  button: {
+    height: 50,
+    width: '100%',
+    backgroundColor: '#6200ea',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+    marginTop: 16,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
 
